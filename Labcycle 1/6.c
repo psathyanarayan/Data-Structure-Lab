c13f32b12ea1b2979cdee4def316910fe9cc7dc7
P Sathya Narayan <60816895+psathyanarayan@users.noreply.github.com>
2020-11-14 12:57:17 +0530

    Update 6.c

--- Labcycle 1/6.c
+++ Labcycle 1/6.c
@@ -2,24 +2,24 @@
 //Insert elements to the Priority Queue.
 //Delete elements from the Priority Queue.
 //Display the contents of the Priority Queue after each operation.
-#include<stdio.h>
-int max,pq_arr[20];
+#include <stdio.h>
+int max, pq_arr[20];
 int front = -1;
 int rear = -1;
 
 void check(int x)
 {
-    int i,j;
-    for (i=0;i<rear;i++)
+    int i, j;
+    for (i = 0; i <= rear; i++)
     {
-        if(x>= pq_arr[i])
+        if (x >= pq_arr[i])
         {
-            for(j=rear+1;j>i;j--)
+            for (j = rear + 1; j > i; j--)
             {
-                pq_arr[j] = pq_arr[j-1];
+                pq_arr[j] = pq_arr[j - 1];
             }
             pq_arr[i] = x;
-            return; 
+            return;
         }
     }
     pq_arr[i] = x;
@@ -27,32 +27,73 @@ void check(int x)
 
 void enqueue(int i)
 {
-    if(rear>=max-1)
+    if (rear > max )
     {
         printf("!! Warning queue Overflow detected !!");
         return;
     }
     if (front == -1 && rear == -1)
     {
-        front+=1;
-        rear+=1;
+        front += 1;
+        rear += 1;
         pq_arr[rear] = i;
         return;
     }
     else
     {
         check(i);
-        rear+=1;
+        rear += 1;
     }
-    
-    
 }
+
+void dequeue(int data)
+{
+    int i;
+    if (front == -1 && rear == -1)
+    {
+        printf("!! Warning Queue is Empty !!");
+    }
+    for (i = 0; i < rear; i++)
+    {
+        if (data == pq_arr[i])
+        {
+            for (; i < rear; i++)
+            {
+                pq_arr[i] = pq_arr[i + 1];
+            }
+            pq_arr[i] = -99;
+            rear--;
+            if (rear == -1)
+            {
+                front = -1;
+            }
+            return;
+        }
+    }
+    printf("\n%d not found in queue to delete", data);
+}
+void display()
+{
+    if ((front == -1) && (rear == -1))
+    {
+        printf("\nQueue is empty");
+        return;
+    }
+ 
+    for (; front <= rear; front++)
+    {
+        printf(" %d ", pq_arr[front]);
+    }
+ printf("\n");
+    front = 0;
+}
+
 int main()
 {
     int item, n;
     printf("Expected MAX Size of the array --> ");
     scanf("%d", &max);
-    
+
     do
     {
         printf("---------------------------------------\n");
@@ -69,10 +110,12 @@ int main()
         case 1:
             printf("Input the element for insertion in queue : ");
             scanf("%d", &item);
-            push(item);
+            enqueue(item);
             break;
         case 2:
-            pop();
+            printf("Input the element for deletion in queue : ");
+            scanf("%d", &item);
+            dequeue(item);
             break;
         case 3:
             display();
